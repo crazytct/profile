84f40b8bf4119476073cfe2f8ac4be62e274c4fb
crazytct <156101760+crazytct@users.noreply.github.com>
2024-05-01 20:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
+const bot1 = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot1.on("error",(err) => {
+  console.error(err);
+}
+bot.connect();
+bot1.connect();
